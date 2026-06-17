6da7b7031585ab9b84b61e5f1c77a940cda5a724
Yester <yester.zhng@gmail.com>
2026-06-16 22:43:47 -0400

    New Learning Program

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,101 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Red", 5);
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        Circle circle = new Circle("Green", 3);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        for (int i = 0; i < shapes.Count; i++)
+        {
+            Console.WriteLine($"Color: {shapes[i].GetColor()}");
+            Console.WriteLine($"Area: {shapes[i].GetArea()}");
+            Console.WriteLine();
+        }
     }
 }
+
+public class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+}
+
+public class Square : Shape
+{
+    private double _side;
+
+    public Square(string color, double side) : base(color)
+    {
+        _side = side;
+    }
+
+    public override double GetArea()
+    {
+        return _side * _side;
+    }
+}
+
+public class Rectangle : Shape
+{
+    private double _length;
+    private double _width;
+
+    public Rectangle(string color, double length, double width) : base(color)
+    {
+        _length = length;
+        _width = width;
+    }
+
+    public override double GetArea()
+    {
+        return _length * _width;
+    }
+}
+
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius) : base(color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+}
+
+
+
